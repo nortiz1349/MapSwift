719d3ca48cadf9e3cd3f1296113d570d5291a382
Nortiz <nortiz1349@gmail.com>
2022-10-12 22:14:23 +0900

    map annotation marker added

--- MapSwift/Views/LocationsView.swift
+++ MapSwift/Views/LocationsView.swift
@@ -14,26 +14,14 @@ struct LocationsView: View {
 	
 	var body: some View {
 		ZStack {
-			Map(coordinateRegion: $vm.mapRegion)
+			mapLayer
 				.ignoresSafeArea()
 			
 			VStack(spacing: 0) {
 				header
 					.padding()
 				Spacer()
-				
-				ZStack {
-					ForEach(vm.locations) { location in
-						if vm.mapLocation == location {
-							LocationPreviewView(location: location)
-								.shadow(radius: 20)
-								.padding()
-								.transition(.asymmetric(
-									insertion: .move(edge: .trailing),
-									removal: .move(edge: .leading)))
-						}
-					}
-				}
+				locationsPreviewStack
 			}
 		}
 	}
@@ -68,7 +56,7 @@ extension LocationsView {
 							.rotationEffect(Angle(degrees:vm.showLocationsList ? 180 : 0))
 					}
 			}
-
+			
 			if vm.showLocationsList {
 				LocationsListView()
 			}
@@ -77,5 +65,37 @@ extension LocationsView {
 		.background(.thickMaterial)
 		.cornerRadius(10)
 		.shadow(radius: 20, x: 0, y: 15)
+	}
+	
+	private var mapLayer: some View {
+		Map(coordinateRegion: $vm.mapRegion,
+			annotationItems: vm.locations,
+			annotationContent: { location in
+			//				MapMarker(coordinate: location.coordinates, tint: .blue) // 디폴트 맵 마커
+			MapAnnotation(coordinate: location.coordinates) {
+				LocationMapAnnotationView()
+					.scaleEffect(vm.mapLocation == location ? 1 : 0.7)
+					.shadow(radius: 10)
+					.onTapGesture {
+						vm.showNextLocation(location: location)
+					}
+				
+			}
+		})
+	}
+	
+	private var locationsPreviewStack: some View {
+		ZStack {
+			ForEach(vm.locations) { location in
+				if vm.mapLocation == location {
+					LocationPreviewView(location: location)
+						.shadow(radius: 20)
+						.padding()
+						.transition(.asymmetric(
+							insertion: .move(edge: .trailing),
+							removal: .move(edge: .leading)))
+				}
+			}
+		}
 	}
 }
