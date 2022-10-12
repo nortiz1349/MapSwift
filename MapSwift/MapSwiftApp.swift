74114989ae4c3af595ccb55670c33c49bfc607d7
Nortiz <nortiz1349@gmail.com>
2022-10-12 11:05:03 +0900

    Initial Commit

---
+++ MapSwift/MapSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapSwiftApp.swift
+//  MapSwift
+//
+//  Created by Nortiz M1 on 2022/10/12.
+//
+
+import SwiftUI
+
+@main
+struct MapSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
