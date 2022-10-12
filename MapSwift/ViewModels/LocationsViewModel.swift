0e5843ebef16dd33589975dc5729c082f4c28e3b
Nortiz <nortiz1349@gmail.com>
2022-10-12 21:39:49 +0900

    map preview added

--- MapSwift/ViewModels/LocationsViewModel.swift
+++ MapSwift/ViewModels/LocationsViewModel.swift
@@ -56,4 +56,27 @@ class LocationsViewModel: ObservableObject {
 			showLocationsList = false
 		}
 	}
+	
+	func nextButtonPressed() {
+		
+		// Get the current Index
+		guard let currentIndex = locations.firstIndex(where: { $0 == mapLocation }) else {
+			print("Could not find current index in locations array! Should never happen")
+			return
+		}
+		
+		// Check if the nextIndex is valid
+		let nextIndex = currentIndex + 1
+		guard locations.indices.contains(nextIndex) else {
+			// Next index is Not valid
+			// Restart from 0
+			guard let firstLocation = locations.first else { return }
+			showNextLocation(location: firstLocation)
+			return
+		}
+		
+		// Next index is Valid
+		let nextLocation = locations[nextIndex]
+		showNextLocation(location: nextLocation)
+	}
 }
